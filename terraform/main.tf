4d84986a92f9a94654e7c5da5351929cd8c81ab0
André Veiga <a.branco.veiga@gmail.com>
2022-07-23 00:58:57 +0100

    preparing to generate the Ansible inventory as a Terraform Output

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,11 +22,43 @@ data "civo_disk_image" "ubuntu" {
   }
 }
 
-# Create a new instance
-resource "civo_instance" "server" {
-  count      = 1
+# Create the first K8s Control Plane node
+resource "civo_instance" "first_server" {
   region     = "LON1"
   size       = element(data.civo_size.large.sizes, 0).name
   disk_image = element(data.civo_disk_image.ubuntu.diskimages, 0).id
   sshkey_id  = var.sshkey_id
+}
+
+# Create the other K8s Control Plane node
+resource "civo_instance" "other_servers" {
+  count      = 0
+  region     = "LON1"
+  size       = element(data.civo_size.large.sizes, 0).name
+  disk_image = element(data.civo_disk_image.ubuntu.diskimages, 0).id
+  sshkey_id  = var.sshkey_id
+}
+
+resource "civo_instance" "workers" {
+  count      = 0
+  region     = "LON1"
+  size       = element(data.civo_size.large.sizes, 0).name
+  disk_image = element(data.civo_disk_image.ubuntu.diskimages, 0).id
+  sshkey_id  = var.sshkey_id
+}
+
+# data "civo_instance" "server" {
+#   name       = civo_instance.server.name
+#   depends_on = [civo_instance.server]
+# }
+
+resource "local_file" "ansible_inventory" {
+  content = yamlencode(templatefile("inventory.tftpl",
+    {
+      first_server_ip  = civo_instance.first_server.public_ip,
+      other_servers_ip = civo_instance.other_servers.*.public_ip
+      workers_ip       = civo_instance.workers.*.public_ip
+    }
+  ))
+  filename = "inventory"
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,9 +4,18 @@ terraform {
       source  = "civo/civo"
       version = "1.0.20"
     }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.2.3"
+    }
   }
 }
 
 provider "civo" {
   token = var.token
+}
+
+provider "local" {
+  # Configuration options
 }
